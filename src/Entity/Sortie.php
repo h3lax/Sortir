28f8696ea497a9454bc7cd911a54bbd5d00d5e67
Lucie Legault <LucieLouise@air-de-lucie-2.home>
2023-11-15 14:01:03 +0100

    sortie

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -78,7 +78,10 @@ class Sortie
      */
     private $lieu;
 
-  
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $motClef;
 
     public function __construct()
     {
@@ -114,12 +117,12 @@ class Sortie
         return $this;
     }
 
-    public function getDuree(): ?\DateInterval
+    public function getDuree(): ?int
     {
         return $this->duree;
     }
 
-    public function setDuree(\DateInterval $duree): self
+    public function setDuree(int $duree): self
     {
         $this->duree = $duree;
 
@@ -237,6 +240,16 @@ class Sortie
         return $this;
     }
 
-    
+    public function getMotClef(): ?string
+    {
+        return $this->motClef;
+    }
+
+    public function setMotClef(?string $motClef): self
+    {
+        $this->motClef = $motClef;
+
+        return $this;
+    }
 
 }
