42336dd14d2485cfc7bae393649940e334c370f6
Rautureau Baptiste <rautureau.baptiste@hotmail.fr>
2023-11-16 13:30:56 +0100

    modifier profil OK sans la photo

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -8,6 +8,7 @@ use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParticipantController extends AbstractController
@@ -31,25 +32,29 @@ class ParticipantController extends AbstractController
     /**
      * @Route("/participant/modifierprofil", name="participant_modifierprofil")
      */
-    public function modifierProfil(Request $request): Response
+    public function modifierProfil(Request $request, UserPasswordHasherInterface $mdpHasher): Response
     {
-        $participant = $this->getUser();
-
-
-        $modifierForm = $this->createForm(ModifierProfilType::class, $participant);
+        $modifierForm = $this->createForm(ModifierProfilType::class, $this->getUser());
         $modifierForm->handleRequest($request);
 
         if ($modifierForm->isSubmitted() && $modifierForm->isValid()) {
+            $participant = $modifierForm->getData();
+            $motPasse = $modifierForm->get('motPasse')->getData();
+
+            //hashe le MDP
+            if (null !== $motPasse){
+            $participant->setMotPasse(
+                $mdpHasher->hashPassword( $participant, $motPasse ) );
+            }
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->flush();
 
-                $this->addFlash('success', 'Profil mis à jour avec succès.');
+            $this->addFlash('success', 'Profil mis à jour avec succès.');
 
-                 return $this->redirectToRoute('accueil');
+                 //return $this->redirectToRoute('participant_modifierprofil');
              }
         return $this->render('participant/modifierProfil.html.twig', [
-            'participant' => $participant,
             'modifierForm' => $modifierForm->createView(),
         ]);
         }
