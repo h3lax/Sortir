b46c4ddb9d46ffc5298a546c2efa639e4da4e23b
Rautureau Baptiste <rautureau.baptiste@hotmail.fr>
2023-11-22 15:00:34 +0100

    modif service ChangeEtat

--- src/Services/ChangeEtat.php
+++ src/Services/ChangeEtat.php
@@ -3,20 +3,38 @@
 namespace App\Services;
 
 use App\Entity\Sortie;
+use App\Repository\EtatRepository;
 use App\Repository\SortieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+use PhpParser\Node\Expr\Array_;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class ChangeEtat
+class ChangeEtat extends AbstractController
 {
-//Rechercher les sorties "passées" pour lesquelles la date de début + durée est inférieure à la date/heure courante plus 1 mois et les passer en "historisée"
-    public function Archive(SortieRepository $sortieRepository, EntityManager $entityManager, Request $request){
-
-        $sortieRepository = $entityManager->getRepository(Sortie::class);
-
-        $sortieArchive = $sortieRepository->findBy(['etat' => '35'])
+//Rechercher les sorties "ouvertes" pour lesquelles la date de clôture est inférieure ou égale à la date du jour (ou nombre max de participants atteints) et les passer en "clôturé"
+    public function cloturerSorties (ArrayCollection $sorties, EntityManagerInterface $entityManager,EtatRepository $etatRepository)
+    {
+        if ()
 
     }
+
+
+//Rechercher les sorties "clôturées" pour lesquelles la date de début de sortie est inférieure ou égale à la date/heure courante et les passer en "en cours"
+
+
+
+//Rechercher les sorties "en cours" pour lesquelles la date de début + durée est inférieure ou égale à la date/heure courante et les passer en "passé"
+
+
+
+//Rechercher les sorties "passées" pour lesquelles la date de début + durée est inférieure à la date/heure courante plus 1 mois et les passer en "historisée"
+
 
 
 //Rechercher les sorties "annulées" pour lesquelles la date de début + durée est inférieure à la date/heure courante plus 1 mois et les passer en "historisée"
